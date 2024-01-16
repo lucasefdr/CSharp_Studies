bff2052b2333a1bed024e23f1ddabe691f8d9e99
Lucas Eduardo Ferreira da Rosa <lucas.eferreira@totvs.com.br>
2024-01-16 19:26:34 -0300

    Commit

---
+++ WebAPP/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPP;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet("ActionResult")]
+    public ActionResult<string> GetActionResult()
+    {
+        // return "Hello World!";
+        return Ok("Hello World!");
+    }
+
+    [HttpGet("IActionResult")]
+    public IActionResult GetIActionResult()
+    {
+        return Ok("Hello World!");
+    }
+}
